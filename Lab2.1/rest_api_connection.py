7a1cc5646a205984d7f45b17f295fc9d15a7cd63
Alex Seleznev <rossignol.08@yandex.ru>
2022-01-26 14:29:17 +0300

    test z

--- Lab2.1/rest_api_connection.py
+++ Lab2.1/rest_api_connection.py
@@ -1,16 +1,10 @@
 import pprint
 import requests
 
-host_url = '10.31.70.210'
+host_url = 'https://10.31.70.210:55443'
 r = requests.post(host_url + '/api/v1/auth/token-services', auth=("restapi", "j0sg1280-7@"), verify=False)
-token = r.json()[‘token-id’]
-
+token = r.json()['token-id']
 header = {"content-type": "application/json", "X-Auth-Token": token}
+
 r = requests.get(host_url + '/api/v1/interfaces', headers=header, verify=False)
 pprint.pprint(r.json())
-
-{'items': [{'description': '',
-            'icmp-redirects': True,
-            'icmp-unreachable': True,
-            'if-name': 'VirtualPortGroup1',
-            'ip-address': '10.31.70.209',
--- Lab2.1/ssh_connection.py
+++ Lab2.1/ssh_connection.py
@@ -1,6 +1,5 @@
-import pprint
-import requests
-import paramiko, time
+import paramiko
+import time
 
 BUF_SIZE = 2000
 TIMEOUT = 1
@@ -9,7 +8,8 @@ TIMEOUT = 1
 ssh_connection = paramiko.SSHClient()
 ssh_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-ssh_connection.connect('10.31.70.209', username='restapi', password='j0sg1280-7@', look_for_keys=False, allow_agent=False)
+host_url = 'http://10.31.70.209/'
+ssh_connection.connect(host_url, username='restapi', password='j0sg1280-7@', look_for_keys=False, allow_agent=False)
 session = ssh_connection.invoke_shell()
 
 session.send("\n")
